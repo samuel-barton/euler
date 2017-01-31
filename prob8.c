a90a8f655cf3331a2d3fa27cd90d28996fec08d6
Samuel Barton <samuel.barton@maine.edu>
2017-01-31 17:52:50 -0500

    Solved problem 8

--- prob8.c
+++ prob8.c
@@ -1,7 +1,17 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
-
+/*=============================================================================
+ *
+ * Filename: prob8.c
+ *
+ * Project: Euler
+ *
+ * Description: This program calculates the greatest product of 13 adjacent 
+ *              digits in a 1000-digit number, provided in the text file named
+ *              1000_digits. 
+ *
+ *===========================================================================*/
 int main(int ac, char** av)
 {
     unsigned long answer = 0;
@@ -15,23 +25,43 @@ int main(int ac, char** av)
 
     printf("\n");
 
+    int max_index;
     unsigned long product;
-    int i = 0;
-    while (i < 987)
+    int i = 13;
+    while (i < 986)
     {
         product = 1;
         int j = 0;
         while (j < 13)
         {
-            product *= (unsigned long)digits[i+j];
+            /* since we're dealing with characters, we don't check against 0.*/
+            if (digits[i+j] == '0')
+            {
+                i += j;
+                break;
+            }
+
+            /* type casting a char to an integer value gives the ASCII code for
+             * the character, so to get the integer value we just subtract off
+             * the value for the character '0' as the ASCII representation of
+             * decimal digits places their ASCII codes in ascending order from
+             * zero. */
+            product *= (unsigned long)(digits[i+j] - '0');
             j++;
         }
         
         if (product > answer)
+        {
             answer = product;
-
+            max_index = i;
+        }
         i++;
     }
+
+    for (i = 0; i < 12; i++)
+        printf("%c, ", digits[max_index+i]);
+
+    printf("%c\n",digits[max_index+12]);
 
     printf("answer = %lu\n", answer);
 }
