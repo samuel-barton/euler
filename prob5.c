c2829dc7cfec2f2a352fe674d37b6d065bb6b088
Samuel Barton <samuel.barton@maine.edu>
2017-01-31 12:47:26 -0500

    Fixed solution to problem 5 to make it faster.

--- prob5.c
+++ prob5.c
@@ -11,22 +11,27 @@
  *             from 1 to 20.
  *
  *===========================================================================*/
+int test_vals[8] = {20,19,18,17,16,14,13,11};
+
+bool divides(int n)
+{
+    int i = 0;
+    while(i < 8)
+    {
+        if (n % test_vals[i] != 0)
+            return false;
+        i++;
+    }
+
+    return true;
+}
 
 int main(int ac, char** av)
 {
     int value = 2520;
 
-    bool success = false;
-
-    int i;
-    for (i = 1; i < 20; i++)
-    {
-        if (value % i != 0)
-        {
-            value++;
-            i = 0;
-        }
-    }
+    while (!divides(value))
+        value++;
 
     printf("smallest number divisible by all numbers from 1 through 20: %d\n",
            value);
